98711e1a55ee2671a5a985d0e1dc93ad7f11b189
Angus Chapman <angusdotchapman@gmail.com>
2023-02-24 16:34:34 -0500

    publish site with tutorials 02.24.23

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Psychological Scaling using MLDS",
+        "excerpt":"In this tutorial, I’ll walk you through how to implement Maximum Likelihood Difference Scaling in R. See (Knoblauch &amp; Maloney, 2012, chap. 7) for full details. This tutorial was written in R, using RMarkdown to generate this webpage. You can access the markdown file here. I make heavy use of...","categories": [],
+        "tags": [],
+        "url": "/tutorials/psychscaling/",
+        "teaser": null
+      }]
