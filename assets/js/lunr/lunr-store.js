405824aabd4e25d202f605f2c5870397b44d6efa
afchapman <angusdotchapman@gmail.com>
2023-03-01 15:07:21 -0500

    build site 03.01.23

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "New blog",
+        "excerpt":"Hi, and thanks for checking out my blog! I finished up my PhD at the end of last year, and recently moved cross-country from San Diego to Boston to start my postdoc. I’ve always had the idea of writing more on this website, and this transition point in my career...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/new-blog/",
+        "teaser": null
+      },{
         "title": "Psychological Scaling using MLDS",
         "excerpt":"In this tutorial, I’ll walk you through how to implement Maximum Likelihood Difference Scaling in R. See (Knoblauch &amp; Maloney, 2012, chap. 7) for full details. This tutorial was written in R, using RMarkdown to generate this webpage. You can access the markdown file here. I make heavy use of...","categories": [],
         "tags": [],
